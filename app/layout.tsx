9a55cdc11aba7ec973dd786852e996c8e758dace
XsenusWork <ilel@list.ru>
2025-09-24 16:14:29 +0700

    feat(app): локализован  заголовок CIN - Навигатор криобластинга

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'CIN',
+  title: 'Навигатор криобластинга',
   description: 'Cryoblasting Industry Navigator',
 };
 
