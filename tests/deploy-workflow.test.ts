f9a66e381a6ecbeca1c4219710298d7453f6650f
XsenusWork <ilel@list.ru>
2026-05-12 17:11:58 +0700

    Упростить синхронизацию проекта перед выкладкой

--- tests/deploy-workflow.test.ts
+++ tests/deploy-workflow.test.ts
@@ -11,7 +11,6 @@ const workflowSource = fs.readFileSync(
 test('GitHub deploy workflow delegates to the resilient library rollout script', () => {
   assert.match(workflowSource, /appleboy\/ssh-action@v1\.2\.0/);
   assert.match(workflowSource, /git fetch origin main/);
-  assert.match(workflowSource, /working tree has local changes; refusing to reset/);
   assert.match(workflowSource, /git reset --hard origin\/main/);
   assert.match(workflowSource, /bash deploy\/library-rollout\.sh/);
   assert.doesNotMatch(workflowSource, /appleboy\/scp-action/);
